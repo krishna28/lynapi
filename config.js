35c5d3bb0b379f31e0f6d87adc5d77211ee6eeb6
krishnakant singh <searchkisu@gmail.com>
2018-03-24 11:32:41 +0530

    updated

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-var username = process.env.username
-var password = process.env.password
+var username = process.env.dbusername || "krishna";
+var password = process.env.dbpassword || "passw0rd";
+var dburi = "mongodb://".concat(username).concat(":").concat(password).concat("@ds123399.mlab.com:23399/pms")	
 module.exports = {
-"databaseUrl":"mongodb://"+username+":"+password+"@ds123399.mlab.com:23399/pms",    
+"databaseUrl":dburi,    
 "port":process.env.PORT || 3000,
 "secretkey":"krishna@123"
 }
